c8e102242742d611c50720758b61ae41888fd831
Jose L Sanchez <joselsanchez@JLS-Computer.local>
2018-03-02 19:11:17 -0800

    fixed app listen

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use("/", indexRoutes);
 app.use("/beards", beardRoutes);
 app.use("/beards/:id/comments", commentRoutes);
 
-app.listen(provess.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Beard Me Server Has Started!");
 });
